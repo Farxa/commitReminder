8a759b0550bf4a90f893981d783d2e7adfb78576
Rahaf <r.abualhassan@gmail.com>
2021-11-21 23:48:11 +0100

    slack api app installed and configured, TO SOLVE: error not_enabled

--- app.js
+++ app.js
@@ -7,7 +7,60 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-// my code
+// my code here
+
+app.event('app_home_opened', async ({ event, client, context }) => {
+    try {
+      /* view.publish is the method that your app uses to push a view to the Home tab */
+      const result = await client.views.publish({
+  
+        /* the user that opened your app's app home */
+        user_id: event.user,
+  
+        /* the view object that appears in the app home*/
+        view: {
+          type: 'home',
+          callback_id: 'home_view',
+  
+          /* body of the view */
+          blocks: [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome to your _App's Home_* :tada:"
+              }
+            },
+            {
+              "type": "divider"
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+              }
+            },
+            {
+              "type": "actions",
+              "elements": [
+                {
+                  "type": "button",
+                  "text": {
+                    "type": "plain_text",
+                    "text": "Click me!"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  });
 
 
 
